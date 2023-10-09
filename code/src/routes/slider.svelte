146dcb77dd281543acae3432cccdd93990660aba
senn59 <74777070+senn59@users.noreply.github.com>
2023-10-09 14:54:02 +0200

    add hue and saturation sliders

--- code/src/routes/slider.svelte
+++ code/src/routes/slider.svelte
@@ -1,30 +1,21 @@
 <script lang="ts">
+    import { createEventDispatcher } from "svelte";
+    const dispatch = createEventDispatcher<{"rangeChange": {name: string, value: number}}>();
     export let name: string = "";
     export let min: number = 0;
     export let max: number = 100;
-
-    let value = 0;
-    const handleDrag = () => {
-        console.log(value)
+    export let value = 0;
+    const handleInput = () => {
+        dispatch("rangeChange", {name: name, value: value})
     }
 </script>
 
 <h2>{value}</h2>
 <div class="slider">
-    <input type="range" bind:value on:input={handleDrag} id="slider-{name}" min="{min}" max="{max}">
+    <input type="range" bind:value on:input={handleInput} id="slider-{name}" min="{min}" max="{max}">
     <label for="range">{name}</label>
 </div>
 
 <style lang="scss">
-    /* .slider-slider {
-        width: 100px;
-        height: 10px;
-        background: black;
-    }
-    .slider-hover {
-        height: 20px;
-        width: 10px;
-        background-color: yellow;
-        cursor: ew-resize;
-    } */
+
 </style>
