b5816bf154ce24776b52ef23b3bf0edee3001ed4
senn59 <74777070+senn59@users.noreply.github.com>
2023-10-10 21:26:00 +0200

    refactored code in order to programatically add sliders instead of hardcoding them

--- code/src/routes/slider.svelte
+++ code/src/routes/slider.svelte
@@ -1,18 +1,13 @@
 <script lang="ts">
-    import { createEventDispatcher } from "svelte";
-    const dispatch = createEventDispatcher<{"rangeChange": {name: string, value: number}}>();
     export let name: string = "";
     export let min: number = 0;
     export let max: number = 100;
     export let value = 0;
-    const handleInput = () => {
-        dispatch("rangeChange", {name: name, value: value})
-    }
 </script>
 
 <h2>{value}</h2>
 <div class="slider">
-    <input type="range" bind:value on:input={handleInput} id="slider-{name}" min="{min}" max="{max}">
+    <input type="range" bind:value id="slider-{name}" min="{min}" max="{max}">
     <label for="range">{name}</label>
 </div>
 
