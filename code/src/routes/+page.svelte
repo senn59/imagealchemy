313b9c5d1b1ee6035f16a8f57ae261d674b13f9c
senn59 <senn3004@gmail.com>
2023-10-10 21:26:00 +0200

    refactored code in order to programatically add sliders instead of hardcoding them

--- code/src/routes/+page.svelte
+++ code/src/routes/+page.svelte
@@ -1,28 +1,54 @@
 <script lang="ts">
     import Slider from "./slider.svelte";
+
+    interface Filter {
+        value: number,
+        default: number,
+        min: number,
+        max: number,
+        suffix: string
+    };
     
-    let values: {[key: string]: number}  = {
-        "saturation": 100,
-        "hue": 50,
-    }
-    const handleChange = (event: CustomEvent<{name: string, value: number}> ) => {
-        const key = event.detail.name;
-        const value = event.detail.value;
-        values[key] = value;
-        console.log(values);
+    let filters: {[key: string]: Filter}  = {
+        "saturation": {
+            value: 100,
+            default: 100,
+            min: 0,
+            max: 500,
+            suffix: "%",
+        },
+        "hue": {
+            value: 0,
+            default: 0,
+            min: 0,
+            max: 360,
+            suffix: "deg"
+       },
+    };
+
+    let stylesStr: string = ""
+    $: {
+        let styles: Array<string> = [];
+        Object.keys(filters).forEach(filter => {
+            let style: string = `--${filter}:${filters[filter].value}${filters[filter].suffix};`;
+            styles.push(style);
+        })
+        stylesStr = styles.join("");
     }
 </script>
 
 <!-- <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p> -->
-<Slider on:rangeChange={handleChange} name="saturation" value={values["saturation"]}  min={0} max={500}/>
-<Slider on:rangeChange={handleChange} name="hue" value={0}  min={0} max={360}/>
+{#each Object.keys(filters) as filter}
+    <Slider name={filter} bind:value={filters[filter].value}  min={filters[filter].min} max={filters[filter].max}/>
+{/each}
 
-<img src="https://upload.wikimedia.org/wikipedia/commons/b/b6/Image_created_with_a_mobile_phone.png" alt="" style="
---saturation:{values.saturation}%;
---hue:{values.hue}deg;
+<img src="https://upload.wikimedia.org/wikipedia/commons/b/b6/Image_created_with_a_mobile_phone.png" alt="" style="{stylesStr}">
+<!-- <img src="https://upload.wikimedia.org/wikipedia/commons/b/b6/Image_created_with_a_mobile_phone.png" alt="" style="
+--saturation:{filters.saturation.value}%;
+--hue:{filters.hue.value}deg;
 "
->
+> -->
 
 <style>
     img {
