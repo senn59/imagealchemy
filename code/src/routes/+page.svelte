d6f9af56ebc2c7249197bb507b27859e5743d399
senn59 <senn3004@gmail.com>
2023-10-11 09:55:14 +0200

    now also dynamically adds the css properties

--- code/src/routes/+page.svelte
+++ code/src/routes/+page.svelte
@@ -8,16 +8,20 @@
         max: number,
         suffix: string
     };
-    
+   
+   /*
+        Filter names must follow the CSS filter option names as seen below.
+        https://developer.mozilla.org/en-US/docs/Web/CSS/filter
+   */
     let filters: {[key: string]: Filter}  = {
-        "saturation": {
+        "saturate": {
             value: 100,
             default: 100,
             min: 0,
             max: 500,
             suffix: "%",
         },
-        "hue": {
+        "hue-rotate": {
             value: 0,
             default: 0,
             min: 0,
@@ -26,35 +30,25 @@
        },
     };
 
-    let stylesStr: string = ""
+    let style: string = ""
     $: {
         let styles: Array<string> = [];
         Object.keys(filters).forEach(filter => {
-            let style: string = `--${filter}:${filters[filter].value}${filters[filter].suffix};`;
+            let style: string = `${filter}(${filters[filter].value}${filters[filter].suffix})`;
             styles.push(style);
         })
-        stylesStr = styles.join("");
+        style = `filter: ${styles.join("")};`;
     }
 </script>
 
-<!-- <h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p> -->
 {#each Object.keys(filters) as filter}
     <Slider name={filter} bind:value={filters[filter].value}  min={filters[filter].min} max={filters[filter].max}/>
 {/each}
 
-<img src="https://upload.wikimedia.org/wikipedia/commons/b/b6/Image_created_with_a_mobile_phone.png" alt="" style="{stylesStr}">
-<!-- <img src="https://upload.wikimedia.org/wikipedia/commons/b/b6/Image_created_with_a_mobile_phone.png" alt="" style="
---saturation:{filters.saturation.value}%;
---hue:{filters.hue.value}deg;
-"
-> -->
+<img style={style} src="https://upload.wikimedia.org/wikipedia/commons/b/b6/Image_created_with_a_mobile_phone.png" alt="">
 
 <style>
     img {
         width: 400px;
-        filter: 
-            saturate(var(--saturation)) 
-            hue-rotate(var(--hue));
     }
 </style>
