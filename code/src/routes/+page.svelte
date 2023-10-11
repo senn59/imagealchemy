dd987d352de42f26e471e9e54e7dabdafbb8db88
senn59 <senn3004@gmail.com>
2023-10-11 10:04:00 +0200

    added css prop to be able to display a different name for the css filter

--- code/src/routes/+page.svelte
+++ code/src/routes/+page.svelte
@@ -2,39 +2,43 @@
     import Slider from "./slider.svelte";
 
     interface Filter {
-        value: number,
-        default: number,
-        min: number,
-        max: number,
-        suffix: string
+        value: number;
+        default: number;
+        min: number;
+        max: number;
+        suffix: string;
+        cssFunc: string;
     };
    
-   /*
-        Filter names must follow the CSS filter option names as seen below.
+    /* 
+        Object for adding css filter functions. For more info on available filter functions see the below link.
         https://developer.mozilla.org/en-US/docs/Web/CSS/filter
-   */
+    */
     let filters: {[key: string]: Filter}  = {
-        "saturate": {
+        "saturation": {
             value: 100,
             default: 100,
             min: 0,
             max: 500,
             suffix: "%",
+            cssFunc: "saturate"
         },
-        "hue-rotate": {
+        "hue": {
             value: 0,
             default: 0,
             min: 0,
             max: 360,
-            suffix: "deg"
+            suffix: "deg",
+            cssFunc: "hue-rotate"
        },
     };
 
     let style: string = ""
     $: {
         let styles: Array<string> = [];
-        Object.keys(filters).forEach(filter => {
-            let style: string = `${filter}(${filters[filter].value}${filters[filter].suffix})`;
+        Object.keys(filters).forEach(name => {
+            let filter: Filter = filters[name];
+            let style: string = `${filter.cssFunc}(${filter.value}${filter.suffix})`;
             styles.push(style);
         })
         style = `filter: ${styles.join("")};`;
@@ -47,7 +51,7 @@
 
 <img style={style} src="https://upload.wikimedia.org/wikipedia/commons/b/b6/Image_created_with_a_mobile_phone.png" alt="">
 
-<style>
+<style lang="scss">
     img {
         width: 400px;
     }
