97b051eaa8aad28677f238ba6eb120203c6fbd40
senn59 <74777070+senn59@users.noreply.github.com>
2023-10-13 15:13:43 +0200

    add css reset, add basic site layout change favicon

--- code/src/routes/+page.svelte
+++ code/src/routes/+page.svelte
@@ -1,58 +1,41 @@
 <script lang="ts">
-    import Slider from "./slider.svelte";
-
-    interface Filter {
-        value: number;
-        default: number;
-        min: number;
-        max: number;
-        suffix: string;
-        cssFunc: string;
-    };
-   
-    /* 
-        Object for adding css filter functions. For more info on available filter functions see the below link.
-        https://developer.mozilla.org/en-US/docs/Web/CSS/filter
-    */
-    let filters: {[key: string]: Filter}  = {
-        "saturation": {
-            value: 100,
-            default: 100,
-            min: 0,
-            max: 500,
-            suffix: "%",
-            cssFunc: "saturate"
-        },
-        "hue": {
-            value: 0,
-            default: 0,
-            min: 0,
-            max: 360,
-            suffix: "deg",
-            cssFunc: "hue-rotate"
-       },
-    };
-
-    let style: string = ""
-    $: {
-        let styles: Array<string> = [];
-        Object.keys(filters).forEach(name => {
-            let filter: Filter = filters[name];
-            let style: string = `${filter.cssFunc}(${filter.value}${filter.suffix})`;
-            styles.push(style);
-        })
-        style = `filter: ${styles.join("")};`;
-    }
+    import Sliders from "./sliders.svelte";
+    let imageStyle: string; 
 </script>
 
-{#each Object.keys(filters) as filter}
-    <Slider name={filter} bind:value={filters[filter].value}  min={filters[filter].min} max={filters[filter].max}/>
-{/each}
+<div class="wrapper">
+    <div class="sidebar left">
+        <Sliders bind:imageStyle />
+    </div>
+    <img style={imageStyle} src="https://upload.wikimedia.org/wikipedia/commons/b/b6/Image_created_with_a_mobile_phone.png" alt="">
+    <div class="sidebar right">
+    </div>
+</div>
 
-<img style={style} src="https://upload.wikimedia.org/wikipedia/commons/b/b6/Image_created_with_a_mobile_phone.png" alt="">
 
 <style lang="scss">
+    $grey: #2E2E2E;
+    * {
+        margin: 0;
+        padding: 0;
+    }
     img {
-        width: 400px;
+        width: 800px;
+    }
+    .wrapper {
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+    }
+    .sidebar {
+        width: 20%;
+        height: 100vh;
+    }
+    .left {
+        border-right: #2E2E2E solid 5px;
+    }
+    .right {
+        border-left: #2E2E2E solid 5px;
+        opacity: 100;
     }
 </style>
