5913c6486e23c22a3685d8078b2f5fd34e95ab46
senn59 <74777070+senn59@users.noreply.github.com>
2023-10-13 15:13:43 +0200

    add css reset, add basic site layout change favicon

--- code/src/routes/+layout.svelte
+++ code/src/routes/+layout.svelte
@@ -3,5 +3,9 @@
 </main>
 
 <style lang="scss">
-    @use './global.scss';
+    @use './reset.scss';
+    @import url('https://fonts.googleapis.com/css2?family=Darker+Grotesque:wght@400;500;600;700;800;900&display=swap');
+    main {
+        font-family: 'Darker Grotesque', sans-serif;
+    }
 </style>
---
+++ code/src/routes/sliders.svelte
@@ -0,0 +1,50 @@
+<script lang="ts">
+    import Slider from "./slider.svelte";
+
+    interface Filter {
+        value: number;
+        default: number;
+        min: number;
+        max: number;
+        suffix: string;
+        cssFunc: string;
+    };
+   
+    /* 
+        Object for adding css filter functions. For more info on available filter functions see the below link.
+        https://developer.mozilla.org/en-US/docs/Web/CSS/filter
+    */
+    export let imageStyle: string = ""
+    let filters: {[key: string]: Filter}  = {
+        "saturation": {
+            value: 100,
+            default: 100,
+            min: 0,
+            max: 500,
+            suffix: "%",
+            cssFunc: "saturate"
+        },
+        "hue": {
+            value: 0,
+            default: 0,
+            min: 0,
+            max: 360,
+            suffix: "deg",
+            cssFunc: "hue-rotate"
+       },
+    };
+
+    $: {
+        let styles: Array<string> = [];
+        Object.keys(filters).forEach(name => {
+            let filter: Filter = filters[name];
+            let style: string = `${filter.cssFunc}(${filter.value}${filter.suffix})`;
+            styles.push(style);
+        })
+        imageStyle = `filter: ${styles.join("")};`;
+    }
+</script>
+
+{#each Object.keys(filters) as filter}
+    <Slider name={filter} bind:value={filters[filter].value}  min={filters[filter].min} max={filters[filter].max}/>
+{/each}
