4d843f32930067030d4c56ecfff57d8af0871d0e
senn59 <senn3004@gmail.com>
2023-10-13 15:50:58 +0200

    change window title, add sidebars, add logo, add dummy buttons for layout purposes

--- code/src/routes/+page.svelte
+++ code/src/routes/+page.svelte
@@ -5,7 +5,17 @@
 
 <div class="wrapper">
     <div class="sidebar left">
-        <Sliders bind:imageStyle />
+        <div class="logo">
+            <img src="/logo.png" alt="flask">
+            <h1>ImageAlchemy</h1>
+        </div>
+        <div class="sliders">
+            <Sliders bind:imageStyle />
+        </div>
+        <div class="buttons">
+            <button>Copy</button>
+            <button>Download</button>
+        </div>
     </div>
     <img style={imageStyle} src="https://upload.wikimedia.org/wikipedia/commons/b/b6/Image_created_with_a_mobile_phone.png" alt="">
     <div class="sidebar right">
@@ -15,10 +25,8 @@
 
 <style lang="scss">
     $grey: #2E2E2E;
-    * {
-        margin: 0;
-        padding: 0;
-    }
+    $logoText: #560074;
+    $margin: 100px;
     img {
         width: 800px;
     }
@@ -26,16 +34,44 @@
         display: flex;
         align-items: center;
         justify-content: space-between;
+        background-color: #F4F4F4;
     }
     .sidebar {
         width: 20%;
         height: 100vh;
+        outline: $grey 8px solid;
     }
-    .left {
-        border-right: #2E2E2E solid 5px;
-    }
-    .right {
-        border-left: #2E2E2E solid 5px;
-        opacity: 100;
+    .sidebar, .left {
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+        background: white;
+        .logo {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            padding-top: 50px;
+            width: 100%;
+            h1 {
+                margin-top: 5px;
+                color: #560074;
+                font-size: 48px;
+                font-weight: 900;
+            }
+            img {
+            width: 30%;
+            }
+        }
+        .sliders {
+            height: 100%;
+            width: 100%;
+            display: flex;
+            flex-direction: column;
+            padding: 50px 0;
+        }
+        .buttons {
+            padding-bottom: 50px;
+            width: 100%;
+        }
     }
 </style>
