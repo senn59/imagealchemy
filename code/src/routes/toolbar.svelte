ad8564f269879ebbd09d7f1f2e3e1c6d2562a079
senn59 <74777070+senn59@users.noreply.github.com>
2023-10-14 17:16:14 +0200

    now adds the proper filters and flip direction when downloading

--- code/src/routes/toolbar.svelte
+++ code/src/routes/toolbar.svelte
@@ -1,32 +1,31 @@
 <script lang="ts">
-export let transformStyle: string = "";
-let rotation: number = 0;
-let shouldFlip: boolean = false;
+    export let transformStyle: string = "";
+    let rotation: number = 0;
+    let shouldFlip: boolean = false;
 
-const rotate = () => {
-    const newRotationValue = rotation + 90;
-    rotation = newRotationValue == 360 ? 0 : newRotationValue;
-}
-const flip = () => {
-    //TODO: indicator for if flipped on toolbar css
-    shouldFlip = !shouldFlip;
-    console.log("Flip: " + shouldFlip);
-
-}
-const crop = () => {
-    //To be implemented
-    console.log("Crop");
-}
-$: {
-    let flipStyle: string = "scaleX(1)";
-    let rotationStyle: string = `rotate(${rotation}deg)`;
-    if (shouldFlip) {
-        flipStyle = "scaleX(-1)";
+    const rotate = () => {
+        const newRotationValue = rotation + 90;
+        rotation = newRotationValue == 360 ? 0 : newRotationValue;
     }
-    transformStyle = `transform: ${rotationStyle} ${flipStyle};`;
-    console.log(transformStyle)
-}
+    const flip = () => {
+        //TODO: indicator for if flipped on toolbar css
+        shouldFlip = !shouldFlip;
+        console.log("Flip: " + shouldFlip);
 
+    }
+    const crop = () => {
+        //To be implemented
+        console.log("Crop");
+    }
+    $: {
+        let flipStyle: string = "scaleX(1)";
+        let rotationStyle: string = `rotate(${rotation}deg)`;
+        if (shouldFlip) {
+            flipStyle = "scaleX(-1)";
+        }
+        transformStyle = `transform: ${rotationStyle} ${flipStyle};`;
+        console.log(transformStyle)
+    }
 </script>
 
 <div>
