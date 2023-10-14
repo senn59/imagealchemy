1bd5e0b4e03dd45be2fc807c31bceb9763463968
senn59 <74777070+senn59@users.noreply.github.com>
2023-10-14 11:55:57 +0200

    add (hardcoded) way to export image with filters applied

---
+++ code/src/routes/exports.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+	import { onMount } from "svelte";
+
+    const download = () => {
+        let imageElement: any = document.getElementById("edited");
+        let canvas = document.createElement("canvas");
+        let ctx: CanvasRenderingContext2D | null = canvas.getContext("2d");
+        if (ctx == null) {
+            console.log("bad context")
+            return;
+        }
+        canvas.width = imageElement.naturalWidth;
+        canvas.height = imageElement.naturalHeight;
+        ctx.scale(-1, 1);
+        ctx.filter = "sepia(1)";
+        // ctx.clearRect(0,0, canvas.width, canvas.height)
+        // ctx.rotate(0);
+        console.log(imageElement.naturalWidth);
+        console.log(imageElement.naturalHeight);
+        ctx.drawImage(imageElement, -canvas.width, 0);
+        window.open(canvas.toDataURL());
+
+    }
+    const copy = () => {
+        console.log("Copy");
+    }
+</script>
+<button on:click={copy}>Copy</button>
+<button on:click={download}>Download</button>
