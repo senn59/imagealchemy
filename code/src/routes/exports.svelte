3da377fdc1a0b2cc5c32782aef28bde1e15a2663
senn59 <74777070+senn59@users.noreply.github.com>
2023-10-16 22:49:35 +0200

    removing rotation for now, improve code readability.

--- code/src/routes/exports.svelte
+++ code/src/routes/exports.svelte
@@ -1,8 +1,7 @@
 <script lang="ts">
     export let filterStyle: string;
     export let transformStyle: string;
-    let parsedFilterStyle: string;
-    let shouldFlip: boolean = false;
+
     const download = () => {
         let imageElement: any = document.getElementById("edited");
         let canvas = document.createElement("canvas");
@@ -11,30 +10,36 @@
             console.log("bad context")
             return;
         }
-        canvas.width = imageElement.naturalWidth;
-        canvas.height = imageElement.naturalHeight;
-        ctx.filter = parsedFilterStyle;
-        ctx.clearRect(0,0, canvas.width, canvas.height)
-        // ctx.rotate(0);
-        if (shouldFlip) {
-            ctx.scale(-1, 1);
-            ctx.drawImage(imageElement, -canvas.width, 0);
-        } else {
-            ctx.drawImage(imageElement, 0, 0);
-        }
-        window.open(canvas.toDataURL(), "image.png");
+        ctx.filter = getParsedFilters(filterStyle);
+        ctx.translate(canvas.width / 2, canvas.height / 2);
+        ctx.clearRect(0,0, canvas.width, canvas.height);
 
+        if (shouldFlip(transformStyle)) ctx.scale(-1, 1);
+        ctx.drawImage(imageElement, -canvas.width/2, -canvas.height/2);
+        window.open(canvas.toDataURL("image/png"))
+        // let newWindow: Window | null = window.open();
+        // if (newWindow == null) return;
+        // newWindow.document.open();
+        // newWindow.document.write(`<img src="${canvas.toDataURL("image/png")}">`)
     }
     const copy = () => {
         console.log("Copy");
     }
-    $: {
-        if (filterStyle) {
-            parsedFilterStyle = filterStyle.split(" ")[1].slice(0, -1).replace(")", ") ");
+    const getParsedFilters = (styles: string): string => {
+        if (styles) {
+            return styles.split(" ")[1].slice(0, -1).replace(")", ") ");
         }
-        if (transformStyle) {
-            shouldFlip = transformStyle.includes("-1") ? true : false;
+        return ""
+    }
+    const shouldFlip = (styles: string): boolean => {
+        return styles.includes("-1") ? true : false;
+    }
+    const getRotation = (styles: string): number => {
+        if (styles) {
+            let rotationString = transformStyle.split(" ")[1].split("(")[1].split(")")[0].slice(0, -3);
+            return parseInt(rotationString);
         }
+        return 0;
     }
 </script>
 <button on:click={copy}>Copy</button>
