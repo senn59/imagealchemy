4a2644bdea8714ae35543ac2fbaedb6312667e0f
senn59 <74777070+senn59@users.noreply.github.com>
2023-10-29 19:36:34 +0100

    further styled popup

--- code/src/routes/popup.svelte
+++ code/src/routes/popup.svelte
@@ -1,10 +1,21 @@
+<script lang="ts">
+    import Icon from '@iconify/svelte';
+
+    const closeHandler = (e: Event) => {
+        console.log("button clicked");
+    }
+</script>
 <div class="wrapper">
     <div class="box">
         <div>
-            <h1>Welcome to ImageAlchemy!</h1>
+            <div class="close">
+                <button on:click={closeHandler}><Icon icon="octicon:x-12" height=34 /></button>
+            </div>
+            <h1>Welcome to ImageAlchemy! <span><img src="/logo.png" alt="logo"></span></h1>
             <p>
 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur id ipsum eget nisi vulputate maximus. Pellentesque enim dolor, luctus vel erat id, finibus aliquet eros. Cras nisl metus, imperdiet id dictum quis, tincidunt vitae elit. Maecenas et ex nec metus efficitur suscipit sed nec ipsum. Cras vehicula enim vel congue rutrum. Donec dolor sem, varius pulvinar sem nec, convallis accumsan dolor.
             </p>
+            <img class="preview" src="/preview.png" alt="website preview">
         </div>
     </div>
 </div>
@@ -19,7 +30,7 @@ Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur id ipsum eget
         align-items: center;
         .box {
             width: 50%;
-            height: 80%;
+            height: fit-content;
             background-color: white;
             box-shadow: $grey 0 0 0 6px;
             border-radius: 25px;
@@ -28,14 +39,37 @@ Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur id ipsum eget
             justify-content: center;
             align-items: center;
             padding: 50px;
+            position: relative;
             h1 {
                 font-size: 3rem;
                 font-weight: 800;
+                img {
+                    height: 2.2rem;
+                    position: relative;
+                    top: 8px;
+                }
             }
             p {
                 font-size: 1.3rem;
                 font-weight: 500;
-                padding: 20px 0;
+                padding: 20px 0 26px 0;
+            }
+            & > div > img {
+                width: calc(100% - 12px);
+                box-shadow: $grey 0 0 0 6px;
+            }
+            .close {
+                position: absolute;
+                right: -15px;
+                top: -15px;
+                button {
+                    border: 0;
+                    background-color: red;
+                    padding: 6px;
+                    border-radius: 50px;
+                    box-shadow: black 0 0 0 6px;
+                    cursor: pointer;
+                }
             }
         }
     }
